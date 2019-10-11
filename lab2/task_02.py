97399e4f2fe06a4e1dd48dbef6eb30b9d3b365a2
Myralllka <Olli25010@gmail.com>
2019-10-12 01:38:23 +0300

    add second lab

--- lab2/task_02.py
+++ lab2/task_02.py
@@ -13,21 +13,120 @@ class Point:
         return math.sqrt((self.x - other.x) ** 2 +
                          (self.y - other.y) ** 2)
 
+    def perimeter(self, other1, other2):
+        if self == other2 or self == other1 or other2 == other1:
+            return 2 ** 18
+        a = self.distance_to(other1)
+        b = self.distance_to(other2)
+        c = other1.distance_to(other2)
+
+        if a + b > c or a + c > b or b + c > a:
+            return a + b + c
+        else:
+            return 2 ** 18
+
     def __str__(self):
         return self.__repr__()
 
     def __repr__(self):
-        return "Point({}, {})".format(self.x, self.y)
+        return "{} {}".format(self.x, self.y)
 
     def __hash__(self):
         return hash(self.x)
 
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
 
-def solution(a):
+
+def minimal_perimeter(in_lst):
+    a = []
+    i = 0
+    while i <= len(a):
+        a.append(Point(in_lst[i], in_lst[i + 1]))
+        i += 2
+
     ax = sorted(a, key=lambda x: x.x)
     ay = sorted(a, key=lambda x: x.y)
-    p1, p2, minimum_value = closest_pair(ax, ay)
-    return minimum_value, p1, p2
+    p1, p2, p3, minimum_value = closest_triple(ax, ay)
+    return "{} {} {}".format(minimum_value, p1, p2)
+
+
+def closest_triple(px, py):
+    length = len(px)
+    if length <= 5:
+        return check_five_dots(px)
+    middle = length // 2
+    qx = px[middle:]
+    rx = px[:middle]
+    setq = set(qx)
+    qy = []
+    ry = []
+    for x in py:
+        if x in setq:
+            qy.append(x)
+        else:
+            ry.append(x)
+    (p1, q1, r1, min_val_1) = closest_triple(qx, qy)
+    (p2, q2, r2, min_val_2) = closest_triple(rx, ry)
+    if min_val_1 <= min_val_2:
+        d = min_val_1
+        mn = (p1, q1, r1)
+    else:
+        d = min_val_2
+        mn = (p2, q2, r2)
+    (p3, q3, r3, min_val_3) = closest_split_triple(px, py, d, mn)
+    if d <= min_val_3:
+        return mn[0], mn[1], mn[2], d
+    return p3, q3, r3, min_val_3
+
+
+def check_five_dots(subarr_x):
+    p1, p2, p3 = subarr_x[0], subarr_x[1], subarr_x[2]
+    min_val = p1.perimeter(p2, p3)
+    length = len(subarr_x)
+    if length == 3:
+        return p1, p2, p3, min_val
+    for i in range(length - 2):
+        for j in range(i + 1, length):
+            if i and j != 1:
+                d = subarr_x[i].perimeter(subarr_x[i + 1], subarr_x[i + 2])
+                if d < min_val:
+                    min_val = d
+                    p1, p2, p3 = subarr_x[i], subarr_x[i + 1], subarr_x[i + 2]
+    return p1, p2, p3, min_val
+
+
+def closest_split_triple(p_x, p_y, delta, best_triple):
+    len_x = len(p_x)
+    max_x = p_x[len_x // 2].x
+    list_y = [x for x in p_y if max_x - delta <= x.x <= max_x + delta]
+    best = delta
+    len_y = len(list_y)
+    for i in range(len_y - 1):
+        for j in range(i + 1, min(i + 7, len_y)):
+            for k in range(j + 1, min(j + 7, len_y)):
+                p, q, r = list_y[i], list_y[j], list_y[k]
+                perimeter = p.perimeter(q, r)
+                if perimeter < best:
+                    best_triple = p, q, r
+                    best = perimeter
+    return best_triple[0], best_triple[1], best_triple[2], best
+
+
+def closest_split_pair(p_x, p_y, delta, best_pair):
+    len_x = len(p_x)
+    max_x = p_x[len_x // 2].x
+    list_y = [x for x in p_y if max_x - delta <= x.x <= max_x + delta]
+    best = delta
+    len_y = len(list_y)
+    for i in range(len_y - 1):
+        for j in range(i + 1, min(i + 7, len_y)):
+            p, q = list_y[i], list_y[j]
+            distance = p.distance_to(q)
+            if distance < best:
+                best_pair = p, q
+                best = distance
+    return best_pair[0], best_pair[1], best
 
 
 def check_three_dots(subarr_x):
@@ -47,22 +146,6 @@ def check_three_dots(subarr_x):
     return p1, p2, min_value
 
 
-def closest_split_pair(p_x, p_y, delta, best_pair):
-    ln_x = len(p_x)
-    mx_x = p_x[ln_x // 2].x
-    s_y = [x for x in p_y if mx_x - delta <= x.x <= mx_x + delta]
-    best = delta
-    ln_y = len(s_y)
-    for i in range(ln_y - 1):
-        for j in range(i + 1, min(i + 7, ln_y)):
-            p, q = s_y[i], s_y[j]
-            dst = p.distance_to(q)
-            if dst < best:
-                best_pair = p, q
-                best = dst
-    return best_pair[0], best_pair[1], best
-
-
 def closest_pair(px, py):
     length = len(px)
     if length <= 3:
@@ -78,7 +161,6 @@ def closest_pair(px, py):
             qy.append(x)
         else:
             ry.append(x)
-
     (p1, q1, min_val_1) = closest_pair(qx, qy)
     (p2, q2, min_val_2) = closest_pair(rx, ry)
     if min_val_1 <= min_val_2:
@@ -87,22 +169,35 @@ def closest_pair(px, py):
     else:
         d = min_val_2
         mn = (p2, q2)
-
     (p3, q3, min_val_3) = closest_split_pair(px, py, d, mn)
     if d <= min_val_3:
         return mn[0], mn[1], d
-    else:
-        return p3, q3, min_val_3
+    return p3, q3, min_val_3
+
+
+def closest_pair(in_lst):
+    a = []
+    i = 0
+    while i <= len(a):
+        a.append(Point(in_lst[i], in_lst[i + 1]))
+        i += 2
+    ax = sorted(a, key=lambda x: x.x)
+    ay = sorted(a, key=lambda x: x.y)
+    p1, p2, minimum_value = closest_pair(ax, ay)
+    return "{} {} {}".format(minimum_value, p1, p2)
 
 
 def input(filename):
     result = []
-    with open(filename, 'r') as ff:
-        for each in ff.readlines():
-            (x, y) = list(map(int, each.split()))
-            result.append(Point(x, y))
-    # print(result)
+    ff = open(filename, 'r')
+    n = ff.readline()
+    for each in ff.readlines():
+        (x, y) = list(map(int, each.split()))
+        result.append(Point(x, y))
+    ff.close()
     return result
 
 
-print(solution(input('input_100.txt')))
+if __name__ == "__main__":
+    print(closest_pair(input('input_100.txt')))
+    print(minimal_perimeter(input('input_100.txt')))
