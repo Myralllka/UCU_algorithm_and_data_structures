5ed692ee33e0960edcb0abd82b97057db3bec5ce
Mykola <Olli25010@gmail.com>
2019-11-03 00:01:28 +0200

    start lab 4

---
+++ lab4/task_04.py
@@ -0,0 +1,19 @@
+class HashTable:
+    def __init__(self, hash_type, values):
+        self.hash_type = hash_type
+        self.values = values
+
+    def get_collision_amount(self):
+        pass
+
+    def find_sum(self):
+        pass
+
+
+class HashFunction:
+    def __init__(self):
+        self.table = list()
+
+    def __hash__(self):
+        pass
+
