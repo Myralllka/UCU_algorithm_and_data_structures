8aaa2622f09716582c830188015413780700a794
Mykola <Olli25010@gmail.com>
2019-12-06 19:37:12 +0200

    lab 6

--- lab5/task_05.py
+++ lab5/task_05.py
@@ -1,10 +1,96 @@
-def knapsack(array: list):
+import json
+
+
+def knapsack(items: list, total_weight):
     """
 
-    :param array:
+    :param total_weight:
+    :param items: (value, weight)
     :return:
     """
-    total_value = 0
-    result_items = 0
+    if len(items) >= 500:
+        return 0, 0
+    result_items = []
+    size = (total_weight, len(items))
+    res = [[0 for i in range(size[0] + 1)] for j in range(size[1] + 1)]
+    for i in range(size[1] + 1):
+        for x in range(size[0] + 1):
+            if i == 0 or x == 0:
+                res[i][x] = 0
+            elif items[i - 1][1] <= x:
+                tmp1 = res[i - 1][x]
+                tmp2 = res[i - 1][x - items[i - 1][1]] + items[i - 1][0]
+                tmp3 = max(tmp1, tmp2)
+                res[i][x] = tmp3
+            else:
+                res[i][x] = res[i - 1][x]
 
+    ######
+    total_value = res[size[1]][size[0]]
+    i = size[1]
+    while i > 0:
+        if res[i][total_weight] > res[i - 1][total_weight]:
+            result_items.append(i - 1)
+            total_weight -= items[i - 1][1]
+        i -= 1
+    ######
     return total_value, result_items
+
+
+dct = {}
+
+
+def knapsack_new(items: list, total_weight, n=0):
+    """
+    :param total_weight:
+    :param items: (value, weight)
+    :return:
+    """
+    if total_weight < 0:
+        return -999999999
+    if n < 0 or total_weight == 0:
+        return 0
+    key = str(len(items)) + '|' + str(total_weight)
+
+    if not key in dct:
+        tmp = items[len(items)] + knapsack_new(items, total_weight, n)
+
+    size = (total_weight, len(items))
+    res = [[0 for i in range(size[0] + 1)] for j in range(size[1] + 1)]
+    for i in range(size[1] + 1):
+        for x in range(size[0] + 1):
+            if i == 0 or x == 0:
+                res[i][x] = 0
+            elif items[i - 1][1] <= x:
+                tmp1 = res[i - 1][x]
+                tmp2 = res[i - 1][x - items[i - 1][1]] + items[i - 1][0]
+                tmp3 = max(tmp1, tmp2)
+                res[i][x] = tmp3
+            else:
+                res[i][x] = res[i - 1][x]
+
+    ######
+    total_value = res[size[1]][size[0]]
+    i = size[1]
+    while i > 0:
+        if res[i][total_weight] > res[i - 1][total_weight]:
+            result_items.append(i - 1)
+            total_weight -= items[i - 1][1]
+        i -= 1
+    ######
+    return total_value, result_items
+
+
+if __name__ == '__main__':
+    with open('tmp.json') as file:
+        data = json.loads(file.read())
+        items1 = data[0]['items']
+        items2 = data[1]['items']
+        res1 = data[0]['result']
+        res2 = data[1]['result']
+        weight1 = data[0]['weight']
+        weight2 = data[1]['weight']
+        print(knapsack(items2, weight2))
+        print(knapsackbitmask(items2, weight2))
+        # print(data[0])
+        # print(data[1])
--- lab6/task_06.py
+++ lab6/task_06.py
@@ -1,2 +1,60 @@
+import copy
+
+
+class Node:
+    def __init__(self, value, coordinates=(-1, -1), typee=None):
+        """
+        typee = vin, vout, contour;
+        value = 0 or 1;
+        """
+        self.out_node = None
+        self.value = value
+        self.neighbours = []
+        self.dots = []
+        self.typee = typee
+        self.coordinates = coordinates
+
+    def add_neighbor_in(self, node):
+        if self.typee == "vout":
+            raise IndexError("FUCK")
+        self.neighbours.append(node)
+
+    def add_neighbour_out(self, node):
+        if self.typee == "vin":
+            raise IndexError("FUCK")
+        self.neighbours.append(node)
+
+    def __eq__(self, other):
+        return self.coordinates == other.coordinates and self.typee == \
+               other.typee
+
+
 def escape_problem(lst):
-    pass
+
+    def matrix_to_graph(matr):
+        st_node = Node(1, typee="vout")
+        en_node = Node(0, typee="vin")
+        for i in range(len(matr)):
+            for j in range(len(matr)):
+                if i in contour or j in contour:
+                    end_node.add_neighbor_in(matrix[i][j])
+                    if matrix[i][j].value == 1:
+                        start_node.add_neighbour_out(matrix[i][j])
+        return start_node, end_node
+
+    contour = [0, len(lst) - 1]
+    neigbours = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+    matrix = copy.deepcopy(lst)
+
+    """
+    Make a matrix of Nodes
+    """
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            matrix[i][j] = Node(matrix[i][j], (i, j))
+
+    start_node, end_node = matrix_to_graph(matrix)
+
+
+if __name__ == "__main__":
+    escape_problem([[1, 0, 0, 0], [1, 0, 0, 1], [0, 1, 1, 0], [0, 1, 0, 0]])
